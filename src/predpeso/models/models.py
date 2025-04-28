fef4493551a0a5f7f80d37534928970044475874
Willians-S-S <09willians@gmail.com>
2025-04-28 14:30:49 -0300

    Feat: Associar user a farm

--- src/predpeso/models/models.py
+++ src/predpeso/models/models.py
@@ -24,16 +24,21 @@ class UserModel(Base):
     created_at: Mapped[datetime] = mapped_column(nullable=False)
     updated_at: Mapped[datetime] = mapped_column(nullable=False)
 
+    farm_associations: Mapped[list["UserFarmAssociation"]] = relationship(
+        "UserFarmAssociation", 
+        back_populates="user", 
+        cascade="all, delete-orphan", 
+        passive_deletes=True 
+    )
+
     def __repr__(self):
         return f"<User id={self.id} username='{self.username}'>"
     
 class UserFarmAssociation(Base):
-    _tablename_ = 'user_farm_association'
+    __tablename__ = 'user_farm_association'
 
     user_id: Mapped[str] = mapped_column(ForeignKey('user.id', ondelete="CASCADE"), primary_key=True)
     farm_id: Mapped[str] = mapped_column(ForeignKey('farm.id', ondelete="CASCADE"), primary_key=True)
-
-    role: Mapped[UserFarmRole] = mapped_column(SQLEnum(UserFarmRole, name="user_farm_role_enum", create_type=False), nullable=False)
 
     user: Mapped["UserModel"] = relationship(back_populates="farm_associations")
     farm: Mapped["FarmModel"] = relationship(back_populates="user_associations")
@@ -43,7 +48,7 @@ class UserFarmAssociation(Base):
     
 
 class FarmModel(Base):
-    _tablename_ = 'farm'
+    __tablename__ = 'farm'
 
     id: Mapped[str] = mapped_column(primary_key=True, unique= True)
     name: Mapped[str] = mapped_column(nullable=False)
