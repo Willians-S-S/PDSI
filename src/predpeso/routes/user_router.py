8890ac290f1c321063c555ee8436758d3b93fb14
Willians S. Santos <09willians@gmail.com>
2025-05-17 16:00:01 -0300

    Update

--- src/predpeso/routes/user_router.py
+++ src/predpeso/routes/user_router.py
@@ -14,29 +14,30 @@ from predpeso.security.jwt_token import get_current_user
 user_router = APIRouter(prefix='/user')
 
 @user_router.post("/", response_model=UserResponse)
-def creat_user(name: str = Form(...), 
-               username: str = Form(...), 
-               email: str = Form(...), 
-               password: str = Form(...), 
-               cpf: str = Form(...), 
-               role: UserFarmRole = Form(...), 
-               image: UploadFile = File(...), 
+def creat_user(name: str = Form(...),
+               username: str = Form(...),
+               email: str = Form(...),
+               password: str = Form(...),
+               cpf: str = Form(...),
+               role: UserFarmRole = Form(...),
+               image: UploadFile = File(...),
                db: Session = Depends(get_db) ):
-    
-    return UserService(db_session=db).add(name=name, 
-                                          username=username, 
-                                          email=email, 
-                                          password=password, 
+
+    return UserService(db_session=db).add(name=name,
+                                          username=username,
+                                          email=email,
+                                          password=password,
                                           cpf=cpf,
                                           role=role,
                                           image=image)
 
+@user_router.get("/all")
 def get_all(db: Session = Depends(get_db)):
     return UserService(db_session=db).get_all()
 
 @user_router.put("/{id_user}", response_model=UserResponse)
-def upadate_user(user: UserUpdate, 
-                 id_user: str, 
+def upadate_user(user: UserUpdate,
+                 id_user: str,
                  db: Session = Depends(get_db),
                  current_user: UserModel = Depends(get_current_user)):
     return UserService(db_session=db).update(user=user, user_id=id_user, current_user=current_user)
